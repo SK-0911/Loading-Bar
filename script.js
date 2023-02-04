89a200ccc42b158b65fbfdb8ed83f7da2f91a7d0
SK-0911 <shubham.911k@gmail.com>
2023-02-04 11:16:33 +0530

    Addes the final animation

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const counter = document.querySelector(".counter")
+var counter = document.querySelector(".counter")
 
 const bar = document.querySelector(".loading-front")
 
+const loadingText = document.querySelector(".loading-txt");
+loadingText.innerText = "Loading";
+document.querySelector(".container").insertBefore(loadingText, counter);
+
 let idx = 0;
 
-function updateNum(){
+let dots = 0;
 
+updateNum()
+
+function updateNum(){
+    counter.innerText = idx + "%";
+    bar.style.width = idx + "%";
+    if(idx===100){
+        counter.classList.add("green-text");
+        loadingText.innerText = "Loading";
+    }
+    else {
+        loadingText.innerText = "Loading" + ".".repeat(dots % 4);
+        dots++;
+    }
+    idx++;
+    if(idx < 101){
+        setTimeout(updateNum, 40)
+    }
 }
+
+
